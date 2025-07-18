d89ce76ab7424659dd8aa38c033ffab04f3119de
Bruno Albuquerque <126733239+BrunoAlbuquerqu3@users.noreply.github.com>
2025-07-18 20:12:54 -0300

    Refactor Curso class to initialize Alunos list and remove nullable type from Nome property

--- Propriedades, métodos e construtores/Models/Curso.cs
+++ Propriedades, métodos e construtores/Models/Curso.cs
@@ -7,6 +7,16 @@ namespace Propriedades__métodos_e_construtores.Models
 {
     public class Curso
     {
-        
+        public string Nome { get; set; }
+        list<Pessoa> Alunos { get; set; }
+
+        public void AdicionarAluno(Pessoa aluno)
+        {
+            if (Alunos == null)
+            {
+                Alunos = new List<Pessoa>();
+            }
+            Alunos.Add(aluno);
+        }
     }
 }
